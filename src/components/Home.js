d8f911e88234ee36644d52a6a7513fdddc7e73f8
Veronica Adler <vlynnadler@gmail.com>
2021-11-08 23:55:51 -0500

    tutorial #15 completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'; 
+import { useState, useEffect } from 'react'; 
+import BlogList from './BlogList';
 
 const Home = () => {
 
@@ -8,16 +9,18 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+        console.log('use effect ran')
+    });
 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-               <div className="blog-preview" key={ blog.id }>
-                   <h2> { blog.title }</h2>
-                   <p>Written by { blog.author }</p>
-
-               </div> 
-            ))}
+            <BlogList blogs={blogs} handleDelete={handleDelete} />
         </div>  
     );
 }
