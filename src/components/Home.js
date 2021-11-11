17ff5d6e1765dd46c43fe1719a663bc71ab865c4
Veronica Adler <vlynnadler@gmail.com>
2021-11-11 13:00:20 -0500

    tutorial #19 error handling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,22 +3,35 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([null]);
+    const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+          fetch('http://localhost:8000/blogs')
           .then(res => {
+              if(!res.ok) {
+                throw Error('could not fetch data for that resource.')
+              }
             return res.json();
           })
           .then(data => {
             setBlogs(data);
             setIsPending(false);
-          });
+            setError(null);
+          })
+          .catch(err => {
+              // auto catches network / connection error
+              setIsPending(false);
+              setError(err.message)
+          })
+        }, 1000);
       }, []);
 
     return (
         <div className="home">
+            { error && <div>{ error }</div>}
             { isPending && <div>Loading...</div>} {/*only runs if the fetch is pending*/}
             {blogs && <BlogList blogs={blogs} />} {/*only runs once the blogs data is retrieved from the json server*/}
         </div>  
