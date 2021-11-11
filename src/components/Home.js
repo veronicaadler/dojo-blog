f6abe4eaa1392384fae88c25553fa4ff8208c9ff
Veronica Adler <vlynnadler@gmail.com>
2021-11-11 12:43:50 -0500

    tutorial #18 completed.  Set up json server

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,24 +3,24 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState([null]);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        console.log('use effect ran')
-    });
+        fetch('http://localhost:8000/blogs')
+          .then(res => {
+            return res.json();
+          })
+          .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+          });
+      }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} handleDelete={handleDelete} />
+            { isPending && <div>Loading...</div>} {/*only runs if the fetch is pending*/}
+            {blogs && <BlogList blogs={blogs} />} {/*only runs once the blogs data is retrieved from the json server*/}
         </div>  
     );
 }
