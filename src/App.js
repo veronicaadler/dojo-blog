23c7dc2e023f78d3f9b0b8a384a3f392ec4a7e9f
Veronica Adler <vlynnadler@gmail.com>
2021-11-24 23:17:57 -0500

    tutorial completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Create from './components/Create';
+import BlogDetails from './components/BlogDetails';
+import NotFound from './components/NotFound';
 import { BrowserRouter as Router, Route, Switch} from  'react-router-dom'
 
 function App() {
@@ -15,6 +17,12 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*"> {/*catch all path*/}
+              <NotFound />
             </Route>
           </Switch>
         </div>
